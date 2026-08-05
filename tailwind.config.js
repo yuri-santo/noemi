715441afce3025291a6ed1f54a59c498a075a171
yuri geovane <yuri.martiniano@hotmail.com>
2026-08-05 18:58:18 -0300

    feat: adicionar fluxo de acompanhantes e remover tailwind CDN

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./admin.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        cursive: ['"Great Vibes"', 'cursive'],
+        cute: ['"Baloo 2"', 'cursive'],
+      },
+      colors: {
+        'ark-blue': '#87CEEB',
+        'ark-darkblue': '#3B7DD8',
+        'ark-gold': '#D4AF37',
+        'ark-brown': '#6B4423',
+      }
+    }
+  },
+  plugins: [],
+}
